e2d1cf642cfc2acb0729324bf6678d86ab5f0295
C0ffeeMachine <yashshukla195@gmail.com>
2023-02-10 14:32:13 +0530

    fixed properties file

--- addressMS/src/main/java/com/example/addressMS/exception/InvalidCredentialsException.java
+++ addressMS/src/main/java/com/example/addressMS/exception/InvalidCredentialsException.java
@@ -1,6 +1,6 @@
 package com.example.addressMS.exception;
 
-public class InvalidCredentialsException extends Exception{
+public class InvalidCredentialsException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
     public InvalidCredentialsException() {
